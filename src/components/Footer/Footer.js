19476351c8a7a674518319af7692c826b01301e4
Adokiye <gorgeceo@gmail.com>
2020-06-22 17:45:36 +0100

    Tweaks to site

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ReactComponent as TwitterIcon } from '../../assets/svg/twitter.svg';
-import { ReactComponent as FacebookIcon } from '../../assets/svg/facebook.svg';
+import { ReactComponent as LinkedIinIcon } from '../../assets/svg/linkedin.svg';
 import { ReactComponent as InstagramIcon } from '../../assets/svg/instagram.svg';
+import { ReactComponent as GithubIcon } from '../../assets/svg/github.svg';
 
 const Footer = () => {
     return (
@@ -9,8 +10,16 @@ const Footer = () => {
             <div className="container">
                 <div className="row mt-4">
                     <div className="col-md-6 footer__link--box">
+                    <a
+                            href="https://github.com/Adokiye"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="footer__link"
+                        >
+                            <GithubIcon />
+                        </a>
                         <a
-                            href="https://twitter.com"
+                            href="https://twitter.com/seyidev"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="footer__link"
@@ -18,15 +27,15 @@ const Footer = () => {
                             <TwitterIcon />
                         </a>
                         <a
-                            href="https://facebook.com"
+                            href="https://www.linkedin.com/in/iruene-adokiye-oluwaseyifunmi-5537a5163/"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="footer__link"
                         >
-                            <FacebookIcon />
+                            <LinkedIinIcon />
                         </a>
                         <a
-                            href="https://instagram.com"
+                            href="https://www.instagram.com/adokz1/"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="footer__link"
@@ -36,8 +45,7 @@ const Footer = () => {
                     </div>
                     <div className="d-none d-md-block col-md-6">
                         <p className="footer__credits">
-                            <span className="mr-3">Adokiye 2019</span>
-                            <span>Designed by Inioluwa</span>
+                            <span className="mr-3">Adokiye 2020</span>
                         </p>
                     </div>
                 </div>
